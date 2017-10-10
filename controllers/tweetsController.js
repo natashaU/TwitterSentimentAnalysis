903c63e06a6eafe397103b809f653911212e5f17
Natasha Umer <natasha.umer2@gmail.com>
2017-10-10 15:29:04 -0400

    added controller without api call

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -1,1 +1,65 @@
+const tweetDB = require('../models/tweetDB');
+const algorithmia = require("algorithmia");
 
+module.exports = {
+
+
+  index(req, res) {
+    tweetDB.findAll()
+     .then(tweets => {
+       res.json({
+        message: 'ok',
+        data: { tweets },
+       });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json({message: '400', err});
+      });
+    },
+
+   getOne(req, res) {
+    tweetDB.findById(req.params.id)
+    .then(tweet => {
+      res.json({
+        message: 'ok',
+        data: { tweet },
+      });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json({message: '400', err});
+      });
+    },
+
+   create(req, res) {
+    tweetDB.save({
+      twitter_handle: req.body.twitter_handle,
+      positive: req.body.positive,
+      negative: req.body.negative,
+    })
+    .then(score => {
+      res.json({message: 'ok', data: { tweet }});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({message: '400', err});
+    });
+  },
+
+
+   destroy(req, res) {
+    tweetDB.destroy(req.params.id)
+    .then(() => {
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json({message: '400', err});
+      });
+    },
+
+   showTweetForm: (req, res) => {
+    res.json({
+      message: 'Im HTML for new tweets',
+    });
+   },
+}
