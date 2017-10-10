9eee850f0bd9970751f3152cfe1dc8ae839ceb47
Natasha Umer <natasha.umer2@gmail.com>
2017-10-10 16:01:48 -0400

    added react frontend skeleton

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const logger = require('logger');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
